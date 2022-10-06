639717a4ba0339adee88110898f629bfdbc53771
Vertta Moody <75756974+vertta@users.noreply.github.com>
2022-10-06 01:48:10 -0400

    Made corrections to index.html and app.py, resolved issues with button

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scrape_mars
+
 app = Flask(__name__)
+
 # use flask pymongo to set up the connection to the database
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data_db"
-mongo  = PyMongo(app)
-@app.route("/")
+
+mongo  = PyMongo(app)   # set varible monogo to call PyMongo application
+@app.route("/")     # start at the root of the application
+
 def index():
     # access information for the database
     mars_data= mongo.db.marsData.find_one()
@@ -13,7 +17,7 @@ def index():
 
     return render_template("index.html", mars=mars_data)
 
-@app.route("/scrape")
+@app.route("/scrape")   # call scrape function
 def scrape_all():
    # reference to the database collection (table)
     marsTable = mongo.db.marsData
@@ -23,7 +27,7 @@ def scrape_all():
     
     # call scape script
     mars_data = scrape_mars.scrape_all()
-    #print(mars_data)  # print dicitionary that is returned from all 
+    #print(mars_data)  # print dicitionary that is returned from all  uncomment for troubleshooting
     
     # take the dictionary and load into MongoDb
     marsTable.insert_one(mars_data)
